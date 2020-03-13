f1b43f70940eb1cd524f48aaff0f408e246d8c12
Oscar Shoudt <shoudto@gmail.com>
2020-03-12 20:08:25 -0400

    Done.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,18 +1,18 @@
 class ArticlesController < ApplicationController
   def index
-    @articles = Article.all
+    @articles = Article.all # show all articles
   end
 
   def show
-    @article = Article.find(params[:id])
+    @article = Article.find(params[:id]) # present a certain article
   end
 
   def new
-    @article = Article.new
+    @article = Article.new # set a form for a new article 
   end
 
   def create
-    @article = Article.new
+    @article = Article.new # process of making a new article 
     @article.title = params[:title]
     @article.description = params[:description]
     @article.save
@@ -21,12 +21,12 @@ class ArticlesController < ApplicationController
 
   # add edit and update methods here
   def edit
-    @article = Article.find(params[:id]) 
+    @article = Article.find(params[:id]) # finding a existing article 
   end 
 
   def update
-    @article = Article.find(params[:id])
-    @article.update(title: params[:article][:title], description: params[:article][:description])
-    redirect_to article_path(@article)
+    @article = Article.find(params[:id])  # find the id 
+    @article.update(title: params[:article][:title], description: params[:article][:description]) # update the attributes of the article
+    redirect_to article_path(@article) # Then redirect to the current article
   end 
 end
--- db/migrate/20151230232412_create_articles.rb
+++ db/migrate/20151230232412_create_articles.rb
@@ -1,10 +1,10 @@
 class CreateArticles < ActiveRecord::Migration
   def change
-    create_table :articles do |t|
-      t.string :title
+    create_table :articles do |t| # creating an article table 
+      t.string :title             # creating attributes title and description
       t.text :description
 
-      t.timestamps null: false
+      t.timestamps null: false 
     end
   end
 end
